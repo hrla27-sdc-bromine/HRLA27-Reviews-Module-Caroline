19c780d63839b8740298ee509ebcc15141367ba1
Caroline Cange <carolinecang@gmail.com>
2019-03-06 11:14:20 -0800

    updated the server api request serve the postgreSQL database

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
 const cors = require('cors');
-
+const sequelize = require('sequelize');
 const port = 3005;
 const app = express();
 
 // const Product = require('../database-sql/model.js');
-const {Reviews} = require('../database-sql/model.js');
+const { Reviews } = require('../database-sql/model.js');
 // const Reviews = require('../database/model.js');
 app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
+app.use(parser.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(cors());
 
@@ -19,21 +21,108 @@ app.listen(port, () => console.log(`Listening on port ${port}.`));
 app.get('/reviews/:productId', (req, res) => {
   let { productId } = req.params;
   console.log(productId);
-  Reviews.findAll({ limit:2, where: {"productId": productId} } )
-  .then(data => res.status(200).send(data))
-  .catch(error=> res.status(404).send(error));
+  Reviews.findAll({
+      limit: 2,
+      where: {
+        "productId": productId
+      }
+    })
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
 });
 
-app.get('/reviews/:productId/stats', (req, res)=> {
+app.get('/reviews/:productId/stats', (req, res) => {
   console.log('in stats')
   let { productId } = req.params;
-  Reviews.findAll({ where: {"productId": productId} })
-  .then(data => res.status(200).send(data))
-  .catch(error=> res.status(404).send(error));
+  Reviews.findAll({
+      where: {
+        "productId": productId
+      }
+    })
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
 });
 
 
+app.get('/reviews/:productId/helpful/:n', (req, res) => {
+  console.log('in helpful')
+  let { productId, n } = req.params;
+  Reviews.findAll({
+      limit: (JSON.parse(n)),
+      where: {
+        "productId": productId,
+        yes: {
+          $gt: {
+            $col: 'no'
+          }
+        }
+      }
+    })
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
+});
 
+app.get('/reviews/:productId/relevant', (req, res) => {
+  console.log('in relevant')
+  let { productId } = req.params;
+  Reviews.findAll({
+    // limit: (JSON.parse(n)),
+    where: {
+      "productId": productId,
+      no: { $lt: 20 }
+    }
+  })
+  .then(data => res.status(200).send(data))
+  .catch(error => res.status(404).send(error));
+});
+
+app.get('/reviews/:productId/newest/:n', (req, res) => {
+  let { productId, n } = req.params;
+  Reviews.findAll({ 
+    limit: (JSON.parse(n)),
+    where: {
+      "productId": productId
+    },
+    order: [['date', 'DESC'],],
+  })
+  .then(data => res.status(200).send(data))
+  .catch(error => res.status(404).send(error));
+
+});
+
+app.get('/reviews/:productId/stars/:n', (req, res) => {
+  let {
+    productId,n
+  } = req.params;
+  let { starRating } = req.body;
+  Reviews.findAll({
+      limit: (JSON.parse(n)),
+      where: {
+        "productId": productId,
+        "starRating": starRating
+      },
+      order: [
+        ['starRating', 'DESC'],
+      ],
+    })
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
+    
+  })
+  
+  app.get('/reviews/:productId/more', (req, res) => {
+    console.log('in more')
+    let {
+      productId
+    } = req.params;
+    Reviews.findAll({
+        where: {
+          "productId": productId
+        }
+      })
+      .then(data => res.status(200).send(data))
+      .catch(error => res.status(404).send(error));
+  });
 // app.get('/reviews/:product_id', (req, res) => {
 //   console.log(req.params);
 //   let { product_id } = req.params;
@@ -69,7 +158,6 @@ app.get('/reviews/:productId/stats', (req, res)=> {
 //     .then(data => res.status(200).send(data))
 //     .catch(error => res.status(404).end(error));
 // });
-
 // app.get('/reviews/:review_id/newest/:n', (req, res) => {
 //   let { review_id, n } = req.params;
 //   Reviews.find({ review_id })
@@ -107,16 +195,3 @@ app.get('/reviews/:productId/stats', (req, res)=> {
 //     .then(data => res.status(200).send(data))
 //     .catch(error => res.status(404).send(error));
 // });
-app.get('/reviews/:productId/more', (req, res) => {
-  console.log('in more')
-  let {
-    productId
-  } = req.params;
-  Reviews.findAll({
-      where: {
-        "productId": productId
-      }
-    })
-    .then(data => res.status(200).send(data))
-    .catch(error => res.status(404).send(error));
-});
